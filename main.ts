84c02eeffcd39f27dd74ef17c867f5c104ec8cf7
junyoung1210 <117416797+junyoung1210@users.noreply.github.com>
2022-11-11 10:10:29 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,9 +1,14 @@
+OLED.init(128, 64)
+pins.setPull(DigitalPin.P0, PinPullMode.PullUp)
 let count = 0
-OLED.drawLoading(0)
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P16) == 0) {
         count += 1
+        pins.digitalWritePin(DigitalPin.P6, 90)
+        OLED.drawLoading(count * 5)
+        basic.pause(3000)
+        OLED.clear()
+    } else {
+        pins.digitalWritePin(DigitalPin.P6, 0)
     }
-    OLED.drawLoading(0 * pins.digitalReadPin(DigitalPin.P16))
-    pins.servoWritePin(AnalogPin.P0, 180)
 })
